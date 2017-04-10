5245808d9b1eccccd19ff6340cc822dbf1eb6bbf
Markus Binsteiner <makkus@gmail.com>
2017-04-10 20:59:32 +0800

    fixed download function

--- prepare.sh
+++ prepare.sh
@@ -7,7 +7,6 @@ PEM_CERT_URL="https://raw.githubusercontent.com/makkus/freckles-dev/master/certs
 CRT_CERT_URL="https://raw.githubusercontent.com/makkus/freckles-dev/master/certs/markus.crt"
 
 function download {
-    {
         if command_exists wget; then
             wget -O $2 $1
         elif command_exists curl; then
@@ -16,7 +15,6 @@ function download {
             echo "Could not find 'wget' nor 'curl' to download files. Exiting..."
             exit 1
         fi
-    } >> "$SCRIPT_LOG_FILE"
 }
 
 mkdir -p ~/.pip
