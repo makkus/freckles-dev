4099f1e815ffa30e984a005db76937e291a304fb
Markus Binsteiner <makkus@gmail.com>
2017-04-10 20:09:50 +0800

    Condarc

--- prepare.sh
+++ prepare.sh
@@ -1,1 +1,25 @@
 #!/usr/bin/env bash
+
+PIP_CONF_URL=
+CONDA_CONF_URL=
+
+function download {
+    {
+        if command_exists wget; then
+            wget -O $2 $1
+        elif command_exists curl; then
+            curl -o $2 $1
+        else
+            echo "Could not find 'wget' nor 'curl' to download files. Exiting..."
+            exit 1
+        fi
+    } >> "$SCRIPT_LOG_FILE"
+}
+
+mkdir ~/.pip
+cd ~/.pip
+
+download "$PIP_CONF_URL"
+
+cd ~
+download "$CONDA_CONF_URL"
